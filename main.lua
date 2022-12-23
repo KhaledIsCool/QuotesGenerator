543bccbd2fe6f642459a6ec2c5c89b27298f104f
Khaled <104109423+KhaledIsCool@users.noreply.github.com>
2022-12-23 18:02:46 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 -- // Variables
-local Generation = math.random(1,7)
+local Generation = math.random(1,8)
 local QuoteData = Generation
 
 -- // Quotes Table
@@ -10,7 +10,9 @@ local Quotes = {
     'When the enemy is defended at all points, expect an attack. - Sun Tzu',
     'Words have a magical power. They can bring the greatest happiness or the deepest despair. - Sigmund Freud',
     'Out of your vulnerabilities will come your strength. - Sigmund Freud',
-    'If you know the enemy and know yourself, you need not fear the result of a hundred battles. If you know yourself but not the enemy, for every victory gained you will also suffer a defeat. - Sun Tzu'
+    'If you know the enemy and know yourself, you need not fear the result of a hundred battles. If you know yourself but not the enemy, for every victory gained you will also suffer a defeat. - Sun Tzu',
+    'Let them go deep thinking they won, then shut the gates and set them on fire. - Egyptian Military Generals',
+    'Smart people learn from everything and everyone, average ones from experience, stupid ones have all the answers - Socrates'
 }
 
 -- // Print out quotes
